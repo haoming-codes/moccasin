effda0935f830c5bfce3ae6ec871a607631dbbd6
Haoming Li <61410145+haoming-codes@users.noreply.github.com>
2023-04-20 03:18:00 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="mocassin",
     version="0.0.1",
     description="Checkmate prevents you from OOMing when training big deep neural nets",
-    packages=["mocassin"],  # find_packages()
+    packages=find_packages(exclude=("data","*.egg-info","build")),
     python_requires=">=3.9",
     install_requires=[
         "networkx",
