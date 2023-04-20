ef6dfb03a68913b9f19f7dda7d3018e5acae862a
Haoming Li <61410145+haoming-codes@users.noreply.github.com>
2023-04-20 03:17:40 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ setup(
     version="0.0.1",
     description="Checkmate prevents you from OOMing when training big deep neural nets",
     packages=["mocassin"],  # find_packages()
-    python_requires=">=3.5",
+    python_requires=">=3.9",
     install_requires=[
         "networkx",
         "matplotlib",
         "pandas",
         "ortools",
+        "Jinja2"
     ]
 )
