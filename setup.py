6454d1e88643dbfff857d536c1747d58679ccd55
Haoming Li <61410145+haoming-codes@users.noreply.github.com>
2023-05-01 14:50:39 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="mocassin",
     version="0.0.1",
-    description="Checkmate prevents you from OOMing when training big deep neural nets",
+    description="Moccasin, a constraint programming (CP) method for rematerialization",
     packages=find_packages(exclude=("data","*.egg-info","build")),
     python_requires=">=3.9",
     install_requires=[
