86961bfe2be9a9c8daccaf201ddbcca664180811
haoming <edriclihaoming@hotmail.com>
2023-10-31 01:29:09 -0700

    after

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='moccasin',
     packages=['moccasin'],
-    package_dir={'moccasin':'src'},
+    package_dir={'moccasin':'src/moccasin'},
     version="0.0.1",
     description="Moccasin, a constraint programming (CP) method for rematerialization",
     # packages=find_packages(exclude=("data","*.egg-info","build")),
