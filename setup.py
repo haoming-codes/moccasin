3eb6d633aef37831d1d886d26af72daf48ad67a3
haoming <edriclihaoming@hotmail.com>
2023-10-31 01:21:02 -0700

    after

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mocassin",
+    name='moccasin',
+    packages=['moccasin'],
+    package_dir={'moccasin':'src'},
     version="0.0.1",
     description="Moccasin, a constraint programming (CP) method for rematerialization",
-    packages=find_packages(exclude=("data","*.egg-info","build")),
+    # packages=find_packages(exclude=("data","*.egg-info","build")),
     python_requires=">=3.9",
     install_requires=[
         "networkx",
